70d3c9736997e73b979989d471cb817910cac311
get2gpreet <get2gpreet@gmail.com>
2024-10-14 12:29:59 -0400

    First commit

---
+++ src/main/java/com/codie/GraphqlClientPocApplication.java
@@ -0,0 +1,13 @@
+package com.codie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlClientPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlClientPocApplication.class, args);
+	}
+
+}
